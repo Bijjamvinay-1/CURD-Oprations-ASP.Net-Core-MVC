3c72dcf86389ed64e3ba042b32b14ad4f0759109
Bijjam Vinay <v-bivinay@microsoft.com>
2024-06-14 18:27:01 +0530

    Add project files.

---
+++ MVC Application/Db_context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using MVC_Application.Models;
+
+namespace MVC_Application.Db_context
+{
+    public class ApplicationDbContext : DbContext
+    {
+       public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+        }
+        
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ MVC Application/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+
+using MVC_Application;
+using MVC_Application.Db_context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection1")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Products}/{action=Index}/{id?}");
+
+app.Run();
